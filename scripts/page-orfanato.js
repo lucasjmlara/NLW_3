ec4614cfc51f0e0f3f04ef8cf1b1255d6f157a3d
Lucas Lara <lucasjosemartinsdelara@gmail.com>
2021-06-07 20:43:59 -0300

    Aula 2 - dia 3

---
+++ scripts/page-orfanato.js
@@ -0,0 +1,42 @@
+const options = {
+    dragging: false,
+    touchZoom: false,
+    doubleClickZoom: false,
+    scrollWheelZoom: false,
+    zoomControl: false
+}
+
+const map = L.map("mapid", options).setView([-22.5669492,-47.4057182], 15);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
+    .addTo(map);
+
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+})
+
+L.marker([-22.5669492,-47.4057182], {icon})
+  .addTo(map)
+
+/* image gallery */
+
+function selectImage(event) {
+    const button = event.currentTarget;
+
+    const buttons = document.querySelectorAll(".images button");
+    buttons.forEach(removeActiveClass);
+
+    function removeActiveClass(button) {
+        button.classList.remove("active");
+    }
+
+    const image = button.children[0];
+    const imageContainer = document.querySelector(".orphanages-details > img");
+
+    imageContainer.src = image.src;
+
+    button.classList.add("active");
+}
